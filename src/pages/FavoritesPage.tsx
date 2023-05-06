ffb30858d81e048e556d9c8a40acc4398ea74e8b
VitalkaMihal <vitalimikhalkevich1986@gmail.com>
2023-05-06 20:34:32 +0300

    feat(pages): add Link

--- src/pages/FavoritesPage.tsx
+++ src/pages/FavoritesPage.tsx
@@ -1,5 +1,11 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export const FavoritesPage = () => {
-  return <div>FavoritesPage</div>;
+  return (
+    <div>
+      <h1>FavoritesPage</h1>
+      <Link to={"/"}>home</Link>
+    </div>
+  );
 };
