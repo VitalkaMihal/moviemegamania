9ff78a063b5ae3b94b173223d0744967ed613ea1
VitalkaMihal <vitalimikhalkevich1986@gmail.com>
2023-05-14 19:04:06 +0300

    feat(assets): add icons

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,1 +1,8 @@
-import React from "react";
+import { ReactComponent as Favorites } from "./icon/favorites.svg";
+import { ReactComponent as Home } from "./icon/home.svg";
+import { ReactComponent as Settings } from "./icon/settings.svg";
+import { ReactComponent as Trends } from "./icon/trends.svg";
+import { ReactComponent as PixemaDark } from "./icon/pixema-dark-logo.svg";
+import { ReactComponent as PixemaLigth } from "./icon/pixema-light-logo.svg";
+
+export { Favorites, Home, Settings, Trends, PixemaDark, PixemaLigth };
