e05473252bda7e7b00c38da246288136d18128b2
VitalkaMihal <vitalimikhalkevich1986@gmail.com>
2023-05-14 18:28:05 +0300

    feat(ui): add themes, media, colors and typography

--- src/index.tsx
+++ src/index.tsx
@@ -4,10 +4,12 @@ import { Provider } from "react-redux";
 import { RouterProvider } from "react-router-dom";
 import { router } from "routes";
 import { store } from "store";
+import { GlobalStyles } from "ui";
 
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 root.render(
   <Provider store={store}>
+    <GlobalStyles />
     <RouterProvider router={router} />
   </Provider>,
 );
---
+++ src/ui/colors.ts
@@ -0,0 +1,14 @@
+export enum Colors {
+  PrimaryDark = "#7B61FF",
+  PrimaryLight = "##917CFF",
+  Secondary = "#80858B",
+  Black = "#000000",
+  Dark = "#242426",
+  Graphite = "#323537",
+  While = "#FFFFFF",
+  Light = " #FFFFFF",
+  Error = "#FF5154",
+  Green = "#00A340",
+  Yellow = "#F3A608",
+  Orange = "#F45D2D",
+}
