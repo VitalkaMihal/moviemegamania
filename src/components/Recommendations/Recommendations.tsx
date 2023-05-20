691537c31abbc079093fdaf2e87326068ad7b9bb
VitalkaMihal <vitalimikhalkevich1986@gmail.com>
2023-05-20 22:39:24 +0300

    feat(components): add Recommendations

--- src/components/Recommendations/Recommendations.tsx
+++ src/components/Recommendations/Recommendations.tsx
@@ -1,5 +1,30 @@
-import React from "react";
+import { MovieCard } from "components";
+import React, { useEffect } from "react";
+import { Link, generatePath } from "react-router-dom";
+import { ROUTE } from "routes";
+import { fetchMovies, selectMovies, useAppDispatch, useAppSelector } from "store";
+import { HeaderRecommendation, RecommendationsContainer, StyledRecommendations } from "./styles";
 
 export const Recommendations = () => {
-  return <div>Recommendations</div>;
+  const { movies, isLoading, error } = useAppSelector(selectMovies);
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    dispatch(fetchMovies());
+  }, [dispatch]);
+
+  return (
+    <StyledRecommendations>
+      <HeaderRecommendation>Recommendations</HeaderRecommendation>
+      <RecommendationsContainer>
+        {isLoading && <div>spinner</div>}
+        {error && <div>{error}</div>}
+        {movies.map((movie) => (
+          <Link key={movie.title} to={generatePath(ROUTE.DETAILS, { imdbID: movie.imdbID })}>
+            <MovieCard poster={movie.poster} title={movie.title} type={movie.type} />
+          </Link>
+        ))}
+      </RecommendationsContainer>
+    </StyledRecommendations>
+  );
 };
