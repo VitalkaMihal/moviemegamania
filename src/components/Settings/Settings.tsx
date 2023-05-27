68e126072054a7e1e52147a82167156cba5b01cf
VitalkaMihal <vitalimikhalkevich1986@gmail.com>
2023-05-27 15:59:47 +0300

    feat(app): install react swiper

--- src/components/Settings/Settings.tsx
+++ src/components/Settings/Settings.tsx
@@ -1,10 +1,36 @@
 import React from "react";
-import { HeaderMenu, StyledSettingsMenu } from "./styles";
+import { FormColumn, FormContainer, HeaderMenu, StyledSettingsMenu } from "./styles";
+import { Input } from "components/Input/Input";
 
 export const SettingsMenu = () => {
   return (
     <StyledSettingsMenu>
       <HeaderMenu>Profile</HeaderMenu>
+      <FormContainer>
+        <FormColumn>
+          Name
+          <Input />
+        </FormColumn>
+        <FormColumn>
+          Email
+          <Input />
+        </FormColumn>
+      </FormContainer>
+      <HeaderMenu>Password</HeaderMenu>
+      <FormContainer>
+        <FormColumn>
+          Password
+          <Input />
+        </FormColumn>
+        <FormColumn>
+          New password
+          <Input />
+          Confirm password
+          <Input />
+        </FormColumn>
+      </FormContainer>
+      <HeaderMenu>Color mode</HeaderMenu>
+      <FormContainer>Dark theme</FormContainer>
     </StyledSettingsMenu>
   );
 };
--- src/components/Settings/styles.ts
+++ src/components/Settings/styles.ts
@@ -3,6 +3,7 @@ import { Colors, H2, Media } from "ui";
 
 const StyledSettingsMenu = styled.div`
   width: calc(100% - 607px);
+  color: ${Colors.White};
   ${Media.XXL} {
     width: 100%;
   }
@@ -10,7 +11,22 @@ const StyledSettingsMenu = styled.div`
 
 const HeaderMenu = styled.div`
   ${H2};
-  color: ${Colors.White};
+  margin: 40px 0 20px 0;
 `;
 
-export { StyledSettingsMenu, HeaderMenu };
+const FormContainer = styled.form`
+  display: flex;
+  padding: 40px;
+  border-radius: 10px;
+  background-color: ${Colors.Dark};
+  column-gap: 20px;
+`;
+
+const FormColumn = styled.div`
+  display: flex;
+  flex-direction: column;
+  row-gap: 20px;
+  width: 50%;
+`;
+
+export { StyledSettingsMenu, HeaderMenu, FormContainer, FormColumn };
